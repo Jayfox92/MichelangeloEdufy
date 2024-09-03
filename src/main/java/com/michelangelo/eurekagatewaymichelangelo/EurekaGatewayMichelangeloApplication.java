0f31b90cb885028c0f4f5ed1c7cecc4317ef5efc
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2024-09-03 10:40:53 +0200

    Initial commit

---
+++ src/main/java/com/michelangelo/eurekagatewaymichelangelo/EurekaGatewayMichelangeloApplication.java
@@ -0,0 +1,13 @@
+package com.michelangelo.eurekagatewaymichelangelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaGatewayMichelangeloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGatewayMichelangeloApplication.class, args);
+    }
+
+}
